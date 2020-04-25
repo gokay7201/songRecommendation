e891d69e1b4cfed0f8bfc0be13399ae5e8a84dae
Mehmet Gokay Yildiz <42673110+gokay7201@users.noreply.github.com>
2020-04-25 18:18:25 +0300

    some comments

--- solution.pl
+++ solution.pl
@@ -3,12 +3,6 @@
 % compiling: yes
 % complete: yes
 
-%artist(ArtistName, Genres, AlbumIds).
-%album(AlbumId, AlbumName, ArtistNames, TrackIds).
-%track(TrackId, TrackName, ArtistNames, AlbumName, [Explicit, Danceability, Energy,
-%                                                    Key, Loudness, Mode, Speechiness,
-%                                                    Acousticness, Instrumentalness, Liveness,
-%                                                    Valence, Tempo, DurationMs, TimeSignature]).
 features([explicit-0, danceability-1, energy-1,
           key-0, loudness-0, mode-1, speechiness-1,
        	  acousticness-1, instrumentalness-1,
@@ -27,18 +21,17 @@ filter_features_rec([FeatHead|FeatTail], [Head|Tail], FilteredFeatures) :-
         )
     ).
 
-
  %getArtistTracks(+ArtistName, -TrackIds, -TrackNames).
  getArtistTracks(ArtistName, TrackIds, TrackNames) :- artist(ArtistName,_,Ho),
     getAlbumes(Pro1, Ho,0), getAlbumes(Pro2,Ho,1),
     flatten(Pro1,TrackIds), flatten(Pro2, TrackNames).
-
+%iterating album list
 getAlbumes([],[],_).
 getAlbumes(List,[H|T],X):- album(H,_,_,L),
     getTracks(Lisk,L,X),
     getAlbumes(Bebe,T,X),
     List = [Lisk|Bebe].
-
+%iterating track list of an album
 getTracks([],[],_).
 getTracks(Lisp, [H|T],X):-
     trackData(Y,H,X),
@@ -50,36 +43,34 @@ trackData(Y,H,1):-track(H,Y,_,_,_).
 %albumFeatures(+AlbumId, -AlbumFeatures).
 albumFeatures(AlbumId,AlbumFeatures):-
     album(AlbumId,_,_,T),trackLoop(Hello, T,X), divideLoop(Hello,X,AlbumFeatures).
-
+% to sum up all of features of the tracks of an album and counting them 
 trackLoop(Filter,[H|[]],1):-  track(H,_,_,_,Ved), filter_features(Ved,Filter).
 trackLoop(Result,[Hem|T],Y):- 
     track(Hem,_,_,_,Ded),
     filter_features(Ded,Filtered),
     trackLoop(Res,T, X), Y is X+1,
     sumLoop(Filtered,Res,Result). 
-
+%sum up two lists elements and produce a new list
 sumLoop([],[],[]).
 sumLoop([H1|T1],[H2|T2],L3):-
     Y is H1+H2,
     sumLoop(T1,T2,ReTail),
     L3 = [Y|ReTail].
-
+%divide by constant
 divideLoop([],_,[]).
 divideLoop([Head|Tail], K, Result):-
     Y is Head/K,
     divideLoop(Tail,K,ResultTail),
-    Result = [Y|ResultTail].    
+    Result = [Y|ResultTail]. 
+%multiply by constant       
 multiplyConstant([],_,[]).
 multiplyConstant([Head|Tail], K, Result):-
     Y is Head*K,
     multiplyConstant(Tail,K,ResultTail),
     Result = [Y|ResultTail].
-
-
-%artistFeatures(+ArtistName, -ArtistFeatures) 5 points
+%artistFeatures(+ArtistName, -ArtistFeatures)
 artistFeatures(ArtistName, ArtistFeatures):- 
     artist(ArtistName,_,T),albumLoop(Hello, T,X), divideLoop(Hello,X,ArtistFeatures).
-
 albumLoop(Out,[H|[]],Felt):-
     albumFeatures(H,Filter), album(H,_,_,K),
     length(K, Felt), multiplyConstant(Filter, Felt, Out).
@@ -88,40 +79,36 @@ albumLoop(Result,[Hem|T],Y):-
     album(Hem,_,_,P), length(P,Feel), multiplyConstant(Filtered, Feel, And),
     albumLoop(Res,T, X), Y is X + Feel ,
     sumLoop(And,Res,Result).
-
-
-
+%multiply the elements of two lists and produce a new one
 multiplyLoop([],[],[]).
 multiplyLoop([Head|Tail], [H1|T1], Result):-
     Y is Head*H1,
     multiplyLoop(Tail,T1,ResultTail),
     Result = [Y|ResultTail].
-
-%trackDistance(+TrackId1, +TrackId2, -Score) 5 points
+%trackDistance(+TrackId1, +TrackId2, -Score)
 trackDistance(TrackId1, TrackId2, Score):-
     track(TrackId1,_,_,_,D1), track(TrackId2,_,_,_,D2),
     filter_features(D1,Filtered1),filter_features(D2,Filtered2), distanceFinder(Score, Filtered1,Filtered2).
-
+%distance between two elements
 distanceFinder(Result, X,Y):-
     divideLoop(X,-1,Z), sumLoop(Y,Z,T),
     multiplyLoop(T,T,K), sumElementList(K,A), Result is sqrt(A).    
-
+%probably same as in list library
 sumElementList([], 0).
 sumElementList([Head | Tail], Total) :-
     sumElementList(Tail, Sum1),
     Total = Head + Sum1.
 
-%albumDistance(+AlbumId1, +AlbumId2, -Score) 5 points
+%albumDistance(+AlbumId1, +AlbumId2, -Score)
 albumDistance(AlbumId1, AlbumId2, Score):-
     albumFeatures(AlbumId1,Feature1), albumFeatures(AlbumId2,Feature2), distanceFinder(Score,Feature1,Feature2). 
-%artistDistance(+ArtistName1, +ArtistName2, -Score) 5 points
+%artistDistance(+ArtistName1, +ArtistName2, -Score)
 artistDistance(ArtistName1, ArtistName2, Score):-
     artistFeatures(ArtistName1,Feature1), artistFeatures(ArtistName2,Feature2), distanceFinder(Score,Feature1,Feature2). 
 
-
-%findMostSimilarTracks(+TrackId, -SimilarIds, -SimilarNames) 10 points
+%findMostSimilarTracks(+TrackId, -SimilarIds, -SimilarNames) 
 findMostSimilarTracks(TrackId, SimilarIds, SimilarNames):-
-    findall(X-Y, trackDistance(TrackId,Y,X), Term), sort(0,@=<, Term, [_|T]),
+    findall(X-Y, (trackDistance(TrackId,Y,X),\+(TrackId = Y)), Term), keysort(Term,T),
     showIDs(SimilarIds, T,0), showNames(SimilarNames, T,0).
 showIDs([],_, 30).
 showIDs(Result, [_-M|T], X):-
@@ -131,37 +118,34 @@ showNames(Result, [_-M|T], X):-
     Y is X+1, track(M,B,_,_,_),
     showNames(Ret, T,Y), Result = [B|Ret].
 
-% kendisiyle olan şeyini başka bir çıkarma yöntemi bulmalıyız
-%findMostSimilarAlbums(+AlbumId, -SimilarIds, -SimilarNames) 10 points
+%findMostSimilarAlbums(+AlbumId, -SimilarIds, -SimilarNames) 
 findMostSimilarAlbums(AlbumId, SimilarIds, SimilarNames):-
-    findall(X-Y, albumDistance(AlbumId,Y,X), Term), sort(0,@=<, Term, [_|T]),
+    findall(X-Y, (albumDistance(AlbumId,Y,X), \+(AlbumId = Y)), Term), keysort(Term, T),
     showIDs(SimilarIds, T,0), showAlbumNames(SimilarNames, T,0).
 showAlbumNames([],_, 30).
 showAlbumNames(Result, [_-M|T], X):-
     Y is X+1, album(M,B,_,_),
     showAlbumNames(Ret, T,Y), Result = [B|Ret].
 
-%findMostSimilarArtists(+ArtistName, -SimilarArtists) 10 points
-findMostSimilarArtists(ArtistName, SimilarArtists):-findall(X-Y, artistDistance(ArtistName,Y,X), Term),
-sort(0,@=<, Term, [_|T]), showIDs(SimilarArtists, T,0).
+%findMostSimilarArtists(+ArtistName, -SimilarArtists)
+findMostSimilarArtists(ArtistName, SimilarArtists):-findall(X-Y, (artistDistance(ArtistName,Y,X),\+(ArtistName = Y) ), Term),
+keysort(Term, T), showIDs(SimilarArtists, T,0).
 
 
-%filterExplicitTracks(+TrackList, -FilteredTracks) 5 points
+%filterExplicitTracks(+TrackList, -FilteredTracks)
 filterExplicitTracks(TrackList, FilteredTracks) :-
     findall(X, (member(X,TrackList), track(X,_,_,_,[0|_])), FilteredTracks).
 
-% duplicate olanları yok etmek lazım
-%getTrackGenre(+TrackId, -Genres) 5 points
+%getTrackGenre(+TrackId, -Genres)
 getTrackGenre(TrackId, Genres):- track(TrackId,_,Artists,_,_), artistLoop(Result, Artists),flatten(Result, Mid), list_to_set(Mid,Genres).
 artistLoop([],[]).
 artistLoop(Result, [H|T]):-
     artist(H,K,_), artistLoop(Rem, T),
     Result = [K|Rem].
 
-
-%discoverPlaylist(+LikedGenres, +DislikedGenres, +Features, +FileName, -Playlist) 30 points
+%discoverPlaylist(+LikedGenres, +DislikedGenres, +Features, +FileName, -Playlist)
 discoverPlaylist(LikedGenres, DislikedGenres, Features,FileName, Playlist):- 
-    subGenre(LikedGenres, Other1),subGenre(DislikedGenres, Other2),deneme(Other1, Other2, Res),
+    subGenre(LikedGenres, Other1),subGenre(DislikedGenres, Other2),eliminator(Other1, Other2, Res),
     list_to_set(Res,Res1),distanceLoop(Res1,Features,Final),keysort(Final,Final2) ,showAll(Playlist,Points,Final2,0),
     nameAndArtist(Names,Artists,Playlist),printPhase(FileName,Playlist,Names,Artists,Points).
 
@@ -175,7 +159,7 @@ nameAndArtist(Names, Artists, [H|T]):-
 printPhase(File, Ids, Names,Artists,Points):- open(File, write, Stream),
    writeln(Stream, Ids), writeln(Stream,Names), writeln(Stream,Artists),writeln(Stream,Points), close(Stream).
 
-deneme(Liked,Disliked,Result):-
+eliminator(Liked,Disliked,Result):-
     findall(X,(
         getTrackGenre(X,Genres),
         (isLiked(Genres,Liked), \+ isLiked(Genres,Disliked))
@@ -184,7 +168,6 @@ deneme(Liked,Disliked,Result):-
 distanceLoop([],_,[]).
 distanceLoop([H|T], Features, Result):- track(H,_,_,_,L), distanceLoop(T,Features, Remi),
  filter_features(L,L1), distanceFinder(Rex, Features, L1), Result = [Rex-H|Remi].   
-
 
 isLiked(Genres,Liked):- (member(X,Genres), member(X,Liked)) .
 
